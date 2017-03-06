97a8d9805ea1f130af32e670bb58cab696d7b340
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2017-03-06 13:39:18 +0000

    Maven artifacts for 1.0

--- src/main/java/com/andreamazzarella/http_server/middleware/MiddleWare.java
+++
@@ -1,8 +0,0 @@
-package com.andreamazzarella.http_server.middleware;
-
-import com.andreamazzarella.http_server.request_response.Response;
-import com.andreamazzarella.http_server.request_response.Request;
-
-public interface MiddleWare {
-    Response generateResponseFor(Request request);
-}
